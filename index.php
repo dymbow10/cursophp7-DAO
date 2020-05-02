f04e72b1f976fad10a749dc01ac8090290335430
Matheus <theobest9@gmail.com>
2020-05-02 18:35:23 -0300

    Aula 3

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um user
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//carrega uma lista de users
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista buscando pelo login
+//$search = Usuario::search("jo")
+//echo json_encode($search);
+
+//carrega um user usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","tor");
+echo $usuario;
 
  ?>
